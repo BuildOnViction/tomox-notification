dc20215933d710f178e9de217e7e9d853025239c
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2020-08-04 09:21:11 +0000

    noti to tomowallet topup trade, recall

--- lend.js
+++ lend.js
@@ -1,7 +1,9 @@
 const MongoClient = require('mongodb').MongoClient
 const TomoWallet = require('./services/tomowallet')
+const config = require('config')
+const _ = require('lodash')
 
-const uri = 'mongodb://localhost:27017/tomodex?replicaSet=rs0'
+const uri = config.get('mongodb.uri')
 const insert_pipeline = [
     {
         $match: {
@@ -10,15 +12,153 @@ const insert_pipeline = [
     }
 ]
 
+const update_pipeline = [
+    {
+        $match: {
+            operationType: 'update'
+        }
+    }
+]
+
 async function run() {
+    console.log('Start watching events!!!')
 	let con = await MongoClient.connect(uri, { useUnifiedTopology: true })
-	console.log(uri + ' watching: trades')
-	con.db('tomodex').collection('trades').watch(insert_pipeline)
-		.on('change', data => {
-            await TomoWallet.sendNotify(
-                'xxx',
-                `tomochain:dapp?url=https://dex.tomochain.com`
+	con.db('tomodex').collection('lending_trades').watch(insert_pipeline)
+		.on('change', (data) => {
+            let borrower = data.fullDocument.borrower
+            let investor = data.fullDocument.investor
+
+            TomoWallet.sendNotify(
+                borrower,
+                `tomochain:dapp?url=${config.tomoscan.uri}/lending/trade/${data.fullDocument.hash}`,
+                'TRADE | TomoX P2P Lending',
+                'You have a new p2p lending contract'
             )
+            TomoWallet.sendNotify(
+                investor,
+                `tomochain:dapp?url=${config.tomoscan.uri}/lending/trade/${data.fullDocument.hash}`,
+                'TRADE | TomoX P2P Lending',
+                'You have a new p2p lending contract'
+            )
+		})
+
+	con.db('tomodex').collection('lending_trades').watch(update_pipeline, { fullDocument: 'updateLookup' })
+		.on('change', (data) => {
+            let borrower = data.fullDocument.borrower
+            let investor = data.fullDocument.investor
+            let status = data.fullDocument.status.toUpperCase()
+            
+            if (status !== 'CLOSED' && status !== 'LIQUIDATED') {
+                return false
+            }
+
+            TomoWallet.sendNotify(
+                borrower,
+                `tomochain:dapp?url=${config.tomoscan.uri}/lending/trade/${data.fullDocument.hash}`,
+                'TRADE | TomoX P2P Lending',
+                `Your new p2p lending contract is ${status}`
+            )
+            TomoWallet.sendNotify(
+                investor,
+                `tomochain:dapp?url=${config.tomoscan.uri}/lending/trade/${data.fullDocument.hash}`,
+                'TRADE | TomoX P2P Lending',
+                `Your p2p lending contract is ${status}`
+            )
+		})
+
+    /*
+	con.db('tomodex').collection('lending_repays').watch(insert_pipeline)
+		.on('change', (data) => {
+            let tradeHash = data.fullDocument.hash
+
+            con.db('tomodex').collection('lending_trades').findOne({
+                hash: tradeHash
+            }, (err, result) => {
+                if (err) {
+                    console.log(err)
+                    return false
+                }
+                let borrower = result.borrower
+                let investor = result.investor
+
+                TomoWallet.sendNotify(
+                    borrower,
+                    `tomochain:dapp?url=${config.tomoscan.uri}/lending/repay/${result.hash}`,
+                    'REPAY | TomoX P2P Lending',
+                    'Your p2p lending contract have a new REPAY order'
+                )
+
+                TomoWallet.sendNotify(
+                    investor,
+                    `tomochain:dapp?url=${config.tomoscan.uri}/lending/repay/${result.hash}`,
+                    'REPAY | TomoX P2P Lending',
+                    'Your p2p lending contract have a new REPAY order'
+                )
+
+            })
+		})
+    */
+
+	con.db('tomodex').collection('lending_topups').watch(insert_pipeline)
+		.on('change', (data) => {
+            let tradeHash = data.fullDocument.hash
+
+            con.db('tomodex').collection('lending_trades').findOne({
+                hash: tradeHash
+            }, (err, result) => {
+                if (err) {
+                    console.log(err)
+                    return false
+                }
+                let borrower = result.borrower
+                let investor = result.investor
+
+                TomoWallet.sendNotify(
+                    borrower,
+                    `tomochain:dapp?url=${config.tomoscan.uri}/lending/topup/${result.hash}`,
+                    'TOPUP | TomoX P2P Lending',
+                    'Your p2p lending contract have a new TOPUP order'
+                )
+
+                TomoWallet.sendNotify(
+                    investor,
+                    `tomochain:dapp?url=${config.tomoscan.uri}/lending/topup/${result.hash}`,
+                    'TOPUP | TomoX P2P Lending',
+                    'Your p2p lending contract have a new TOPUP order'
+                )
+
+            })
+		})
+
+	con.db('tomodex').collection('lending_recalls').watch(insert_pipeline)
+		.on('change', (data) => {
+            let tradeHash = data.fullDocument.hash
+
+            con.db('tomodex').collection('lending_trades').findOne({
+                hash: tradeHash
+            }, (err, result) => {
+                if (err) {
+                    console.log(err)
+                    return false
+                }
+                let borrower = result.borrower
+                let investor = result.investor
+
+                TomoWallet.sendNotify(
+                    borrower,
+                    `tomochain:dapp?url=${config.tomoscan.uri}/lending/recalls/${result.hash}`,
+                    'RECALL | TomoX P2P Lending',
+                    'Your p2p lending contract have a new RECALL order'
+                )
+
+                TomoWallet.sendNotify(
+                    investor,
+                    `tomochain:dapp?url=${config.tomoscan.uri}/lending/recalls/${result.hash}`,
+                    'RECALL | TomoX P2P Lending',
+                    'Your p2p lending contract have a new RECALL order'
+                )
+
+            })
 		})
 }
 
--- services/tomowallet.js
+++ services/tomowallet.js
@@ -3,23 +3,23 @@ const Web3 = require('web3')
 const config = require('config')
 const web3 = new Web3()
 
-const sendNotify = async function(address, deeplink) {
+const sendNotify = async function(address, deeplink, title, message) {
     var PRIVATE_KEY = config.get('tomowallet.apiKey')
     var api = config.get('tomowallet.uri')
     var base64Data = Buffer.from(JSON.stringify({
         appName: 'TomoDEX',
-        title: 'test',
-        msg: 'test',
+        title: title,
+        msg: message,
         address: address,
         deepLink: deeplink
     })).toString('base64');
     var signMessage = web3.eth.accounts.sign(base64Data, PRIVATE_KEY)
     var signature = signMessage.signature;
-    var { data } = await axios.post(`${api}/api/services/notification/push`, {
+    var { data } = await axios.post(`${api}/api/services/notification/tomodex`, {
         message: base64Data,
         signature
     });
-    console.log(data)
+    console.log(`SEND notification to=${address} title="${title}" msg="${message}" status="${data.message}"`)
     if (data.error) {
         throw new Error('The application dont have permission for push notification to TomoWallet');
     }
